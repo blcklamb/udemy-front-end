b4c27e244b52528fbcdfa493c88cdd91b2b13d41
blcklamb <whenucan35@gmail.com>
2024-01-22 20:44:53 +0900

    add: 46. 신발을 클릭하고 각 파트 별로 색상 바꾸기 - using raycaster

--- React-Three-Fiber-R3F/e-commerce/src/components/three/ShowRoom.tsx
+++ React-Three-Fiber-R3F/e-commerce/src/components/three/ShowRoom.tsx
@@ -1,12 +1,27 @@
 import * as THREE from "three";
+import { useLoader, useThree } from "@react-three/fiber";
+import { GLTFLoader } from "three/examples/jsm/Addons.js";
 
 export const ShowRoom = () => {
+  const { raycaster } = useThree();
+  const gltf = useLoader(GLTFLoader, "./models/custom.glb");
+
+  const shoesClick = () => {
+    const intersects = raycaster.intersectObjects(gltf.scene.children);
+
+    if (intersects.length > 0) {
+      const clickedObject = intersects[0].object as THREE.Mesh;
+      const firstMaterial =
+        clickedObject.material as THREE.MeshStandardMaterial;
+      const cloneMaterial = firstMaterial.clone();
+
+      cloneMaterial.color.set("red");
+      clickedObject.material = cloneMaterial;
+    }
+  };
   return (
-    <mesh
-      rotation={[0, THREE.MathUtils.degToRad(45), THREE.MathUtils.degToRad(45)]}
-    >
-      <boxGeometry />
-      <meshStandardMaterial />
-    </mesh>
+    <>
+      <primitive object={gltf.scene} onClick={shoesClick} />
+    </>
   );
 };
