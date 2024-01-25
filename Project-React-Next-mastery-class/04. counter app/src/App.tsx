6325667c0c6bd51f23da974acdbf57cc8ca9910c
blcklamb <whenucan35@gmail.com>
2024-01-26 01:50:30 +0900

    refactor: move handler logic - get rid of setState props

--- Project-React-Next-mastery-class/04. counter app/src/App.tsx
+++ Project-React-Next-mastery-class/04. counter app/src/App.tsx
@@ -5,11 +5,15 @@ import Viewer from "./components/Viewer";
 
 function App() {
   const [count, setCount] = useState(0);
+
+  const onClickButton = (value: number) => {
+    setCount((prev) => prev + value);
+  };
   return (
     <>
       <h1>Simple Counter</h1>
       <Viewer count={count} />
-      <Controller onControl={setCount} />
+      <Controller onControl={onClickButton} />
     </>
   );
 }
--- Project-React-Next-mastery-class/04. counter app/src/components/Controller.tsx
+++ Project-React-Next-mastery-class/04. counter app/src/components/Controller.tsx
@@ -1,11 +1,11 @@
 interface ControllerProps {
-  onControl: React.Dispatch<React.SetStateAction<number>>;
+  onControl: (value: number) => void;
 }
 
 const Controller = ({ onControl }: ControllerProps) => {
   const onChange = (e: React.MouseEvent<HTMLButtonElement>) => {
     const eventTarget = e.target as HTMLButtonElement;
-    onControl((prev) => prev + +eventTarget.innerText);
+    onControl(+eventTarget.innerText);
   };
 
   const COUNT_VARIATION = ["-100", "-10", "-1", "+1", "+10", "+100"];
