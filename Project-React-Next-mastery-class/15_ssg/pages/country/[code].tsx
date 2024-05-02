af77a8b053944a415f64d5e42f6ecf584a0ac7b4
blcklamb <whenucan35@gmail.com>
2024-05-02 15:01:11 +0900

    🚩 chapter 16. SSG - getStaticProps - getStaticPaths - fallback

--- Project-React-Next-mastery-class/15_ssg/pages/country/[code].tsx
+++ Project-React-Next-mastery-class/15_ssg/pages/country/[code].tsx
@@ -8,6 +8,9 @@ interface CountryProps {
   country: NaraSearchByCode;
 }
 export default function Country({ country }: CountryProps) {
+  if (!country) {
+    return <div>존재하지 않는 국가입니다.</div>;
+  }
   return (
     <div>
       {country.commonName} {country.officialName}
@@ -17,10 +20,20 @@ export default function Country({ country }: CountryProps) {
 
 Country.Layout = SubLayout;
 
+export const getStaticPaths = async () => {
+  return {
+    paths: [{ params: { code: "ABW" } }, { params: { code: "KOR" } }],
+    fallback: "blocking",
+    // "blocking": paths에 없는 걸 요청하면 server에서 데이터 받아온 뒤 렌더링 완료
+    // false: paths에 없는 걸 요청하면 404,
+    // true: paths에 없는 걸 요청하면 일단 router.isFallback의 화면 보여준 뒤 server에서 받아오면 그때 반영 ,
+  };
+};
+
 interface Params extends ParsedUrlQuery {
   code: string;
 }
-export const getServerSideProps: GetServerSideProps = async (context) => {
+export const getStaticProps: GetServerSideProps = async (context) => {
   const { code } = context.params as Params;
 
   let country = null;
